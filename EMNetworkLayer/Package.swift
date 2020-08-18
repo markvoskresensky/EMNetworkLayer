1c24824994c2e9d7fdd1b8b2964c59ca047b4951
Mark Voskresenskiy <mark.voskresenskiy@touchcast.com>
2020-08-18 11:11:36 +0300

    [*] change Package swift file

--- EMNetworkLayer/Package.swift
+++ EMNetworkLayer/Package.swift
@@ -3,26 +3,31 @@
 
 import PackageDescription
 
+let packageName = "EMNetworkLayer"
+
 let package = Package(
-    name: "EMNetworkLayer",
+    name: packageName,
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "EMNetworkLayer",
-            targets: ["EMNetworkLayer"]),
+            name: packageName,
+            type: .static,
+            targets: [packageName]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "EMNetworkLayer",
-            dependencies: []),
+            name: packageName,
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "EMNetworkLayerTests",
             dependencies: ["EMNetworkLayer"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
